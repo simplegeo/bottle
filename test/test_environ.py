33d417d17af2eda8ece406e4be1798f7aaf50907
David Paleino <dapal@debian.org>
2010-03-31 16:53:08 +0200

    Imported Upstream version 0.8.0

--- test/test_environ.py
+++ test/test_environ.py
@@ -1,14 +1,12 @@
+# -*- coding: utf-8 -*-
 import unittest
 import sys, os.path
 from bottle import request, response
+import tools
+from tools import tob
+import wsgiref.util
 
-if sys.version_info[0] == 2:
-    from StringIO import StringIO
-else:
-    from io import StringIO
-            
 class TestEnviron(unittest.TestCase):
-
     def test_path(self):
         """ Environ: PATH_INFO """ 
         t = dict()
@@ -18,25 +16,44 @@ class TestEnviron(unittest.TestCase):
         t['/bla'] = '/bla'
         t['/bla/'] = '/bla/'
         for k, v in t.iteritems():
-            request.bind({'PATH_INFO': k})
+            request.bind({'PATH_INFO': k}, None)
             self.assertEqual(v, request.path)
-        request.bind({})
+        request.bind({}, None)
         self.assertEqual('/', request.path)
 
+    def test_url(self):
+        """ Environ: URL building """
+        request.bind({'HTTP_HOST':'example.com'}, None)
+        self.assertEqual('http://example.com/', request.url)
+        request.bind({'SERVER_NAME':'example.com'}, None)
+        self.assertEqual('http://example.com/', request.url)
+        request.bind({'SERVER_NAME':'example.com', 'SERVER_PORT':'81'}, None)
+        self.assertEqual('http://example.com:81/', request.url)
+        request.bind({'wsgi.url_scheme':'https', 'SERVER_NAME':'example.com'}, None)
+        self.assertEqual('https://example.com:80/', request.url)
+        request.bind({'HTTP_HOST':'example.com', 'PATH_INFO':'/path', 'QUERY_STRING':'1=b&c=d', 'SCRIPT_NAME':'/sp'}, None)
+        self.assertEqual('http://example.com/sp/path?1=b&c=d', request.url)
 
-    def test_ilength(self):
-        """ Environ: CONTENT_LENGTH """
-        t = dict()
-        t[''] = 0
-        t['0815'] = 815
-        t['-666'] = 0
-        t['0'] = 0
-        t['a'] = 0
-        for k, v in t.iteritems():
-            request.bind({'CONTENT_LENGTH': k})
-            self.assertEqual(v, request.input_length)
-        request.bind({})
-        self.assertEqual(0, request.input_length)
+    def test_dict_access(self):
+        """ Environ: request objects are environment dicts """
+        e = {}
+        wsgiref.util.setup_testing_defaults(e)
+        request.bind(e, None)
+        for k, v in e.iteritems():
+            self.assertTrue(k in request)
+            self.assertTrue(request[k] == v)
+
+    def test_header_access(self):
+        """ Environ: Request objects decode headers """
+        e = {}
+        wsgiref.util.setup_testing_defaults(e)
+        e['HTTP_SOME_HEADER'] = 'some value'
+        request.bind(e, None)
+        request['HTTP_SOME_OTHER_HEADER'] = 'some other value'
+        self.assertTrue('Some-Header' in request.header)
+        self.assertTrue(request.header['Some-Header'] == 'some value')
+        self.assertTrue(request.header['Some-Other-Header'] == 'some other value')
+
 
     def test_cookie(self):
         """ Environ: COOKIES """ 
@@ -45,73 +62,129 @@ class TestEnviron(unittest.TestCase):
         t['a=a; b=b'] = {'a': 'a', 'b':'b'}
         t['a=a; a=b'] = {'a': 'b'}
         for k, v in t.iteritems():
-            request.bind({'HTTP_COOKIE': k})
+            request.bind({'HTTP_COOKIE': k}, None)
             self.assertEqual(v, request.COOKIES)
 
-
-    def test_getpost(self):
-        """ Environ: GET and POST (simple) """ 
-        sq = 'a=a&b=b&c=c'
-        qd = {'a':'a', 'b':'b','c':'c'}
+    def test_get(self):
+        """ Environ: GET data """ 
         e = {}
-        e['wsgi.input']   = sq
-        e['QUERY_STRING'] = sq
-        request.bind(e)
-        self.assertEqual(qd, request.GET)
-        self.assertEqual(qd, request.POST)
-        self.assertEqual(qd, request.params)
-
-    def test_multigetpost(self):
-        """ Environ: GET and POST (multi values) """ 
-        sq = 'a=a&a=1'
-        qd = {'a':['a','1']}
+        e['QUERY_STRING'] = 'a=a&a=1&b=b&c=c'
+        request.bind(e, None)
+        self.assertTrue('a' in request.GET)
+        self.assertTrue('b' in request.GET)
+        self.assertEqual(['a','1'], request.GET.getall('a'))
+        self.assertEqual(['b'], request.GET.getall('b'))
+        self.assertEqual('1', request.GET['a'])
+        self.assertEqual('b', request.GET['b'])
+        
+    def test_post(self):
+        """ Environ: POST data """ 
+        sq = u'a=a&a=1&b=b&c=c'.encode('utf8')
         e = {}
-        e['wsgi.input']   = sq
-        e['QUERY_STRING'] = sq
-        request.bind(e)
-        self.assertEqual(qd, request.GET)
-        self.assertEqual(qd, request.POST)
-        self.assertEqual(qd, request.params)
+        wsgiref.util.setup_testing_defaults(e)
+        e['wsgi.input'].write(sq)
+        e['wsgi.input'].seek(0)
+        e['CONTENT_LENGTH'] = str(len(sq))
+        e['REQUEST_METHOD'] = "POST"
+        request.bind(e, None)
+        self.assertTrue('a' in request.POST)
+        self.assertTrue('b' in request.POST)
+        self.assertEqual(['a','1'], request.POST.getall('a'))
+        self.assertEqual(['b'], request.POST.getall('b'))
+        self.assertEqual('1', request.POST['a'])
+        self.assertEqual('b', request.POST['b'])
 
+    def test_params(self):
+        """ Environ: GET and POST are combined in request.param """ 
+        e = {}
+        wsgiref.util.setup_testing_defaults(e)
+        e['wsgi.input'].write(tob('b=b&c=p'))
+        e['wsgi.input'].seek(0)
+        e['CONTENT_LENGTH'] = '7'
+        e['QUERY_STRING'] = 'a=a&c=g'
+        e['REQUEST_METHOD'] = "POST"
+        request.bind(e, None)
+        self.assertEqual(['a','b','c'], sorted(request.params.keys()))
+        self.assertEqual('p', request.params['c'])
+
+    def test_getpostleak(self):
+        """ Environ: GET and POST sh0uld not leak into each other """ 
+        e = {}
+        wsgiref.util.setup_testing_defaults(e)
+        e['wsgi.input'].write(u'b=b'.encode('utf8'))
+        e['wsgi.input'].seek(0)
+        e['CONTENT_LENGTH'] = '3'
+        e['QUERY_STRING'] = 'a=a'
+        e['REQUEST_METHOD'] = "POST"
+        request.bind(e, None)
+        self.assertEqual(['a'], request.GET.keys())
+        self.assertEqual(['b'], request.POST.keys())
+
+    def test_body(self):
+        """ Environ: Request.body should behave like a file object factory """ 
+        e = {}
+        wsgiref.util.setup_testing_defaults(e)
+        e['wsgi.input'].write(u'abc'.encode('utf8'))
+        e['wsgi.input'].seek(0)
+        e['CONTENT_LENGTH'] = str(3)
+        request.bind(e, None)
+        self.assertEqual(u'abc'.encode('utf8'), request.body.read())
+        self.assertEqual(u'abc'.encode('utf8'), request.body.read(3))
+        self.assertEqual(u'abc'.encode('utf8'), request.body.readline())
+        self.assertEqual(u'abc'.encode('utf8'), request.body.readline(3))
+
+    def test_bigbody(self):
+        """ Environ: Request.body should handle big uploads using files """
+        e = {}
+        wsgiref.util.setup_testing_defaults(e)
+        e['wsgi.input'].write((u'x'*1024*1000).encode('utf8'))
+        e['wsgi.input'].seek(0)
+        e['CONTENT_LENGTH'] = str(1024*1000)
+        request.bind(e, None)
+        self.assertTrue(hasattr(request.body, 'fileno'))        
+        self.assertEqual(1024*1000, len(request.body.read()))
+        self.assertEqual(1024, len(request.body.read(1024)))
+        self.assertEqual(1024*1000, len(request.body.readline()))
+        self.assertEqual(1024, len(request.body.readline(1024)))
+
+    def test_tobigbody(self):
+        """ Environ: Request.body should truncate to Content-Length bytes """
+        e = {}
+        wsgiref.util.setup_testing_defaults(e)
+        e['wsgi.input'].write((u'x'*1024).encode('utf8'))
+        e['wsgi.input'].seek(0)
+        e['CONTENT_LENGTH'] = '42'
+        request.bind(e, None)
+        self.assertEqual(42, len(request.body.read()))
+        self.assertEqual(42, len(request.body.read(1024)))
+        self.assertEqual(42, len(request.body.readline()))
+        self.assertEqual(42, len(request.body.readline(1024)))
+
+class TestMultipart(unittest.TestCase):
     def test_multipart(self):
         """ Environ: POST (multipart files and multible values per key) """
-
-        e = {}
-        e['SERVER_PROTOCOL'] = "HTTP/1.1"
-        e['REQUEST_METHOD'] = 'POST'
-        e['CONTENT_TYPE'] = 'multipart/form-data; boundary=----------------314159265358979323846'
-        e['wsgi.input']  = '------------------314159265358979323846\n'
-        e['wsgi.input'] += 'Content-Disposition: form-data; name=test.txt; filename=test.txt\n'
-        e['wsgi.input'] += 'Content-Type: application/octet-stream; charset=ISO-8859-1\n'
-        e['wsgi.input'] += 'Content-Transfer-Encoding: binary\n'
-        e['wsgi.input'] += 'This is a test.\n'
-        e['wsgi.input'] += '------------------314159265358979323846\n'
-        e['wsgi.input'] += 'Content-Disposition: form-data; name=sample.txt; filename=sample.txt\n'
-        e['wsgi.input'] += 'Content-Type: text/plain; charset=ISO-8859-1\n'
-        e['wsgi.input'] += 'Content-Transfer-Encoding: binary\n'
-        e['wsgi.input'] += 'This is a sample\n'
-        e['wsgi.input'] += '------------------314159265358979323846\n'
-        e['wsgi.input'] += 'Content-Disposition: form-data; name=sample.txt; filename=sample2.txt\n'
-        e['wsgi.input'] += 'Content-Type: text/plain; charset=ISO-8859-1\n'
-        e['wsgi.input'] += 'Content-Transfer-Encoding: binary\n'
-        e['wsgi.input'] += 'This is a second sample\n'
-        e['wsgi.input'] += '------------------314159265358979323846--\n'
-        e['CONTENT_LENGTH'] = len(e['wsgi.input'])
-        e['wsgi.input'] = StringIO(e['wsgi.input'])
-        e['wsgi.input'].seek(0)
-        request.bind(e)
-        self.assertTrue(e['CONTENT_LENGTH'], request.input_length)
-        self.assertTrue('test.txt' in request.POST)
-        self.assertTrue('sample.txt' in request.POST)
-        self.assertEqual('This is a test.', request.POST['test.txt'].file.read())
-        self.assertEqual('test.txt', request.POST['test.txt'].filename)
-        self.assertEqual(2, len(request.POST['sample.txt']))
-        self.assertEqual('This is a sample', request.POST['sample.txt'][0].file.read())
-        self.assertEqual('This is a second sample', request.POST['sample.txt'][1].file.read())
-
- 
-suite = unittest.TestSuite()
-suite.addTest(unittest.makeSuite(TestEnviron))
+        fields = [('field1','value1'), ('field2','value2'), ('field2','value3')]
+        files = [('file1','filename1.txt','content1'), ('file2','filename2.py',u'äöü')]
+        e = tools.multipart_environ(fields=fields, files=files)
+        request.bind(e, None)
+        # File content
+        self.assertTrue('file1' in request.POST)
+        self.assertEqual('content1', request.POST['file1'].file.read())
+        # File name and meta data
+        self.assertTrue('file2' in request.POST)
+        self.assertEqual('filename2.py', request.POST['file2'].filename)
+        # UTF-8 files
+        x = request.POST['file2'].file.read()
+        if sys.version_info >= (3,0,0):
+            x = x.encode('ISO-8859-1')
+        self.assertEqual(u'äöü'.encode('utf8'), x)
+        # No file
+        self.assertTrue('file3' not in request.POST)
+        # Field (single)
+        self.assertEqual('value1', request.POST['field1'])
+        # Field (multi)
+        self.assertEqual(2, len(request.POST.getall('field2')))
+        self.assertEqual(['value2', 'value3'], request.POST.getall('field2'))
 
 if __name__ == '__main__':
     unittest.main()
--- test/test_wsgi.py
+++ test/test_wsgi.py
@@ -6,64 +6,27 @@ import urllib2
 from StringIO import StringIO
 import thread
 import time
+from tools import ServerTestBase, tob
 
-try:
-    import wsgi_intercept
-    import wsgi_intercept.urllib2_intercept
-    wsgi_intercept.urllib2_intercept.install_opener()
-except ImportError:
-    print "WARNING: WSGI tests need wsgi_intercept. Not testing!"
-    wsgi_intercept = None
 
-class MethodRequest(urllib2.Request):
-    ''' Used to create HEAD/PUT/DELETE/... requests with urllib2 '''
-    def set_method(self, method):
-        self.method = method.upper()
-    def get_method(self):
-        return getattr(self, 'method', urllib2.Request.get_method(self))
-
-class WsgiTestBase(unittest.TestCase):
-    def setUp(self):
-        ''' Create a new Bottle app set it as default_app and register it to urllib2 '''
-        self.url = 'http://test:80'
-        self.wsgi = bottle.Bottle()
-        self.oldapp = bottle.default_app()
-        bottle.default_app(self.wsgi)
-        wsgi_intercept.add_wsgi_intercept('test', 80, bottle.default_app)
-
-    def tearDown(self):
-        ''' Recover the olt default_app and remove wsgi_intercept from urllib2 '''
-        wsgi_intercept.remove_wsgi_intercept('test', 80)
-        bottle.default_app(self.oldapp)
-
-    def urlopen(self, url, post=None, method=None):
-        ''' Open a path using urllip2.urlopen and the wsgi_intercept domain '''
-        r = MethodRequest(self.url+url, post)
-        if method:
-            r.set_method('HEAD')
-        try:
-            return urllib2.urlopen(r)
-        except urllib2.HTTPError, e:
-            return e
-
-class TestWsgi(WsgiTestBase):
+class TestWsgi(ServerTestBase):
     ''' Tests for WSGI functionality, routing and output casting (decorators) '''
 
     def test_get(self):
         """ WSGI: GET routes"""
         @bottle.route('/')
         def test(): return 'test'
-        self.assertEqual(404, self.urlopen('/not/found').code)
-        self.assertEqual(404, self.urlopen('/', post="var=value").code)
-        self.assertEqual('test', self.urlopen('/').read())
+        self.assertStatus(404, '/not/found')
+        self.assertStatus(404, '/', post="var=value")
+        self.assertBody('test', '/')
 
     def test_post(self):
         """ WSGI: POST routes"""
         @bottle.route('/', method='POST')
         def test(): return 'test'
-        self.assertEqual(404, self.urlopen('/not/found').code)
-        self.assertEqual(404, self.urlopen('/').code)
-        self.assertEqual('test', self.urlopen('/', post="var=value").read())
+        self.assertStatus(404, '/not/found')
+        self.assertStatus(404, '/')
+        self.assertBody('test', '/', post="var=value")
 
     def test_headget(self):
         """ WSGI: HEAD routes and GET fallback"""
@@ -72,134 +35,95 @@ class TestWsgi(WsgiTestBase):
         @bottle.route('/head', method='HEAD')
         def test2(): return 'test'
         # GET -> HEAD
-        self.assertEqual(404, self.urlopen('/head').code)
+        self.assertStatus(404, '/head')
         # HEAD -> HEAD
-        self.assertEqual(200, self.urlopen('/head', method='HEAD').code)
-        self.assertEqual('', self.urlopen('/head', method='HEAD').read())
+        self.assertStatus(200, '/head', method='HEAD')
+        self.assertBody('', '/head', method='HEAD')
         # HEAD -> GET
-        self.assertEqual(200, self.urlopen('/get', method='HEAD').code)
-        self.assertEqual('', self.urlopen('/get', method='HEAD').read())
+        self.assertStatus(200, '/get', method='HEAD')
+        self.assertBody('', '/get', method='HEAD')
+
+    def test_anymethod(self):
+        self.assertStatus(404, '/any')
+        @bottle.route('/any', method='ANY')
+        def test2(): return 'test'
+        self.assertStatus(200, '/any', method='HEAD')
+        self.assertBody('test', '/any', method='GET')
+        self.assertBody('test', '/any', method='POST')
+        self.assertBody('test', '/any', method='DELETE')
+        @bottle.route('/any', method='GET')
+        def test2(): return 'test2'
+        self.assertBody('test2', '/any', method='GET')
+        @bottle.route('/any', method='POST')
+        def test2(): return 'test3'
+        self.assertBody('test3', '/any', method='POST')
+        self.assertBody('test', '/any', method='DELETE')
 
     def test_500(self):
         """ WSGI: Exceptions within handler code (HTTP 500) """
         @bottle.route('/')
         def test(): return 1/0
-        self.assertEqual(500, self.urlopen('/').code)
-        bottle.default_app().catchall = False
-        self.assertRaises(ZeroDivisionError, self.urlopen, '/')
-        
+        self.assertStatus(500, '/')
 
     def test_503(self):
         """ WSGI: Server stopped (HTTP 503) """
         @bottle.route('/')
         def test(): return 'bla'
-        self.assertEqual(200, self.urlopen('/').code)
-        bottle.default_app().serve = False
-        self.assertEqual(503, self.urlopen('/').code)
-
-    def test_default(self):
-        """ WSGI: default routes """
-        @bottle.route('/')
-        def test2(): return 'test'
-        self.assertEqual(404, self.urlopen('/not/found').code)
-        self.assertEqual(200, self.urlopen('/').code)
-        self.assertEqual('test', self.urlopen('/').read())
-        @bottle.default()
-        def test(): return 'default'
-        self.assertEqual(200, self.urlopen('/not/found').code)
-        self.assertEqual('default', self.urlopen('/not/found').read())
-        self.assertEqual(200, self.urlopen('/').code)
-        self.assertEqual('test', self.urlopen('/').read())
+        self.assertStatus(200, '/')
+        bottle.app().serve = False
+        self.assertStatus(503, '/')
 
     def test_401(self):
         """ WSGI: abort(401, '') (HTTP 401) """
         @bottle.route('/')
         def test(): bottle.abort(401)
-        self.assertEqual(401, self.urlopen('/').code)
+        self.assertStatus(401,'/')
         @bottle.error(401)
         def err(e):
             bottle.response.status = 200
             return str(type(e))
-        self.assertEqual(200, self.urlopen('/').code)
-        self.assertEqual("<class 'bottle.HTTPError'>", self.urlopen('/').read())
+        self.assertStatus(200,'/')
+        self.assertBody("<class 'bottle.HTTPError'>",'/')
 
-    def test_307(self):
-        """ WSGI: redirect (HTTP 307) """
+    def test_303(self):
+        """ WSGI: redirect (HTTP 303) """
         @bottle.route('/')
         def test(): bottle.redirect('/yes')
-        @bottle.route('/yes')
-        def test2(): return 'yes'
-        self.assertEqual(200, self.urlopen('/').code)
-        self.assertEqual('yes', self.urlopen('/').read())
+        self.assertStatus(303, '/')
+        self.assertHeader('Location', 'http://127.0.0.1/yes', '/')
 
-    def test_interrupt(self):
-        """ WSGI: Server stopped (HTTP 503) """
-        @bottle.route('/')
-        def test(): raise KeyboardInterrupt()
-        self.assertRaises(KeyboardInterrupt, self.urlopen, '/')
-
-    def test_casting(self):
-        """ WSGI: Output Casting (strings an lists) """
-        @bottle.route('/str')
-        def test(): return 'test'
-        self.assertEqual('test', self.urlopen('/str').read())
-        @bottle.route('/list')
-        def test2(): return ['t','e','st']
-        self.assertEqual('test', self.urlopen('/list').read())
-        @bottle.route('/empty')
-        def test3(): return []
-        self.assertEqual('', self.urlopen('/empty').read())
-        @bottle.route('/none')
-        def test4(): return None
-        self.assertEqual('', self.urlopen('/none').read())
-        @bottle.route('/bad')
-        def test5(): return 12345
-        self.assertEqual(500, self.urlopen('/bad').code)
-
-    def test_file(self):
-        """ WSGI: Output Casting (files) """
-        @bottle.route('/file')
-        def test(): return StringIO('test')
-        self.assertEqual('test', self.urlopen('/file').read())
-
-
-    def test_unicode(self):
-        """ WSGI: Test Unicode support """
-        @bottle.route('/unicode')
-        def test3(): return u'äöüß'
-        @bottle.route('/unicode2')
-        def test4(): return [u'äöüß']
-        @bottle.route('/unicode3')
-        def test5():
-            bottle.response.content_type='text/html; charset=iso-8859-15'
-            return u'äöüß'  
-        self.assertEqual(u'äöüß'.encode('utf8'), self.urlopen('/unicode').read())
-        self.assertEqual(u'äöüß'.encode('utf8'), self.urlopen('/unicode2').read())
-        self.assertEqual(u'äöüß'.encode('iso-8859-15'), self.urlopen('/unicode3').read())
-
-    def test_json(self):
-        """ WSGI: Autojson feature """
-        @bottle.route('/json')
-        def test(): return {'a': 1}
-        self.assertEqual(r'{"a":1}', ''.join(self.urlopen('/json').read().split()))
-        self.assertEqual('application/json', self.urlopen('/json').info().getheader('Content-Type',''))
+    def test_generator_callback(self):
+        @bottle.route('/yield')
+        def test():
+            bottle.response.headers['Test-Header'] = 'test'
+            yield 'foo'
+        @bottle.route('/yield_nothing')
+        def test2():
+            yield
+            bottle.response.headers['Test-Header'] = 'test'
+        self.assertBody('foo', '/yield')
+        self.assertHeader('Test-Header', 'test', '/yield')
+        self.assertBody('', '/yield_nothing')
+        self.assertHeader('Test-Header', 'test', '/yield_nothing')
 
     def test_cookie(self):
         """ WSGI: Cookies """
         @bottle.route('/cookie')
         def test():
             bottle.response.COOKIES['a']="a"
-            bottle.response.set_cookie('b','b')
-            bottle.response.set_cookie('c','c', path='/')
+            bottle.response.set_cookie('b', 'b')
+            bottle.response.set_cookie('c', 'c', path='/')
             return 'hello'
-        c = self.urlopen('/cookie').info().getheader('Set-Cookie','')
-        c = [x.strip() for x in c.split(',')]
+        try:
+            c = self.urlopen('/cookie')['header'].get_all('Set-Cookie', '')
+        except:
+            c = self.urlopen('/cookie')['header'].get('Set-Cookie', '').split(',')
+            c = [x.strip() for x in c]
         self.assertTrue('a=a' in c)
         self.assertTrue('b=b' in c)
         self.assertTrue('c=c; Path=/' in c)
 
-
-class TestDecorators(WsgiTestBase):
+class TestDecorators(ServerTestBase):
     ''' Tests Decorators '''
 
     def test_view(self):
@@ -208,46 +132,79 @@ class TestDecorators(WsgiTestBase):
         @bottle.view('stpl_t2main')
         def test():
             return dict(content='1234')
-        result='+base+\n+main+\n!1234!\n+include+\n-main-\n+include+\n-base-\n'
-        self.assertEqual('text/html', self.urlopen('/tpl').info().getheader('Content-Type',''))
-        self.assertEqual(result, self.urlopen('/tpl').read())
-    
+        result = '+base+\n+main+\n!1234!\n+include+\n-main-\n+include+\n-base-\n'
+        self.assertHeader('Content-Type', 'text/html; charset=UTF-8', '/tpl')
+        self.assertBody(result, '/tpl')
+
     def test_validate(self):
         """ WSGI: Test validate-decorator"""
         @bottle.route('/:var')
         @bottle.route('/')
         @bottle.validate(var=int)
         def test(var): return 'x' * var
-        self.assertEqual(403, self.urlopen('/noint').code)
-        self.assertEqual(403, self.urlopen('/').code)
-        self.assertEqual(200, self.urlopen('/5').code)
-        self.assertEqual('xxx', self.urlopen('/3').read())
+        self.assertStatus(403,'/noint')
+        self.assertStatus(403,'/')
+        self.assertStatus(200,'/5')
+        self.assertBody('xxx', '/3')
+
+    def test_routebuild(self):
+        """ WSGI: Test validate-decorator"""
+        @bottle.route('/a/:b/c', name='named')
+        def test(var): pass
+        self.assertEqual('/a/xxx/c', bottle.url('named', b='xxx'))
+        self.assertEqual('/a/xxx/c', bottle.app().get_url('named', b='xxx'))
+
+    def test_decorators(self):
+        app = bottle.Bottle()
+        app.route('/g')('foo')
+        bottle.route('/g')('foo')
+        app.route('/g2', method='GET')('foo')
+        bottle.get('/g2')('foo')
+        app.route('/p', method='POST')('foo')
+        bottle.post('/p')('foo')
+        app.route('/p2', method='PUT')('foo')
+        bottle.put('/p2')('foo')
+        app.route('/d', method='DELETE')('foo')
+        bottle.delete('/d')('foo')
+        self.assertEqual(app.routes, bottle.app().routes)
+
+    def test_autoroute(self):
+        app = bottle.Bottle()
+        def a(): pass
+        def b(x): pass
+        def c(x, y): pass
+        def d(x, y=5): pass
+        def e(x=5, y=6): pass
+        self.assertEqual(['a'],list(bottle.yieldroutes(a)))
+        self.assertEqual(['b/:x'],list(bottle.yieldroutes(b)))
+        self.assertEqual(['c/:x/:y'],list(bottle.yieldroutes(c)))
+        self.assertEqual(['d/:x','d/:x/:y'],list(bottle.yieldroutes(d)))
+        self.assertEqual(['e','e/:x','e/:x/:y'],list(bottle.yieldroutes(e)))
 
 
-class TestRun(WsgiTestBase):
-    ''' Tests Running a Server '''
+class TestAppMounting(ServerTestBase):
+    def setUp(self):
+        ServerTestBase.setUp(self)
+        self.subapp = bottle.Bottle()
+    
+    def test_basicmounting(self):
+        bottle.app().mount(self.subapp, '/test')
+        self.assertStatus(404, '/')
+        self.assertStatus(404, '/test')
+        self.assertStatus(404, '/test/')
+        self.assertStatus(404, '/test/test/bar')
+        @self.subapp.route('/')
+        @self.subapp.route('/test/:test')
+        def test(test='foo'):
+            return test
+        self.assertStatus(404, '/')
+        self.assertStatus(404, '/test')
+        self.assertStatus(200, '/test/')
+        self.assertBody('foo', '/test/')
+        self.assertStatus(200, '/test/test/bar')
+        self.assertBody('bar', '/test/test/bar')
 
-    def test_wsgiref(self):
-        """ WSGI: Test wsgiref """
-        @bottle.route('/test')
-        def test():
-            return 'test'
-        def paratest():
-            try:
-                time.sleep(1)
-                self.assertEqual('test', urllib2.urlopen('http://127.0.0.1:61382/test').read())
-            finally:
-                thread.interrupt_main()
-        
-        thread.start_new_thread(paratest, ())
-        bottle.run(port=61382, quiet=True)
 
-
-suite = unittest.TestSuite()
-if wsgi_intercept:
-    suite.addTest(unittest.makeSuite(TestWsgi))
-    suite.addTest(unittest.makeSuite(TestDecorators))
-    suite.addTest(unittest.makeSuite(TestRun))
-
+    
 if __name__ == '__main__':
     unittest.main()
